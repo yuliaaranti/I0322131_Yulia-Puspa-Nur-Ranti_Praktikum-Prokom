6bd3abb8a3ccfddf3187afb35c7c4cce6021f580
yulia <yuliaranty14@gmail.com>
2023-03-06 10:03:50 +0700

    first commit

---
+++ yulia.py
@@ -0,0 +1,13 @@
+nama_depan = "Yulia"
+nama_blkg = "Ranti"
+
+angka = 10
+desimal = 0,5
+boolean = True
+kelas_D = ["yulia", "kls d", "dll"]
+data = {"nama": "yulia", "kelas": "KLS D", "nim" : "131"}
+
+print (nama_depan)
+print ("nama saya adalah Yulia")
+print (f"nama saya adalah {nama_depan} {nama_blkg} dari kelas {data['kelas']} ")
+print (f"nama saya adalah {nama_depan} {nama_blkg} dari {data['kelas'])}")
