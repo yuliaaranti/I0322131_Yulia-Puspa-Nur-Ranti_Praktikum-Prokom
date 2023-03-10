e95b4f05f50cef9bc0a996b04a45f9a21efe6957
yulia <yuliaranty14@gmail.com>
2023-03-10 14:48:47 +0700

    commit exercise-01

---
+++ Exercise 01.py
@@ -0,0 +1,27 @@
+Title = "Exercise 1"
+Nama = "Yulia Puspa Nur Ranti"
+Nim = "I0322131"
+print ()
+print (Title)
+print (Nama)
+print (Nim)
+print()
+print ("\tHalo semuanya!!")
+print('''
+        Perkenalkan, nama saya Yulia Puspa Nur Ranti dengan NIM I0322131. Saya 
+merupakan mahasiswa Teknik Industri angkatan 2022 yang berasal dari Surakarta
+dan tinggal di Karangasem.
+        Tempat kelahiran saya memang bukan di Surakarta, namun saya sudah tinggal
+di sini sejak umur saya 3 tahun. Daerah tempat saya tinggal tidak memiliki 
+banyak perubahan. Tetap ramai karena tempat tinggal saya yang cukup
+strategis, yaitu dekat dengan Pasar Kleco.
+        Sama seperti kota-kota pada umumnya, disini cenderung panas dan banyak
+polusi. Disamping itu semua, pemandangan yang saya senangi salah satunya adalah
+melihat warga sibuk pergi ke pasar pagi-pagi buta, dimana keadaan jalan raya belum 
+terlalu ramai dan suasana yang tenang.
+        Walaupun begitu, saya tetap betah dan senang tinggal di Surakarta. Saya
+juga menikmati kehidupan perkuliahan saya di UNS. Harapan saya kedepannya sebagai
+lulusan Teknik Industri adalah untuk bisa mendapatkan pekerjaan yang berkaitan
+dengan Teknik Industri agar saya dapat menerapkan ilmu yang saya pelajari di
+sini dengan baik.
+''')
